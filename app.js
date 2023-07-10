c5ccb01d67cc14e52864f5f4b0e5ba5371928a69
Selim Kaya <136734877+magnuscarlsencoding@users.noreply.github.com>
2023-07-10 19:55:50 +0300

    app.js

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onClick = () => {
+menuIcon.onclick = () => {
          menuIcon.classList.toggle('bx-x');
          navbar.classList.toggle('active');
 }
-
-
-
-let sections = document.querySelectorAll('section')
-let navLinks = document.querySelectorAll('header nav a')
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
          sections.forEach(sec => {
                   let top = window.scrollY;
-                  let offset = sec.offsetTop - 100;
+                  let offset = sec.offsetTop - 150;
                   let height = sec.offsetHeight;
                   let id = sec.getAttribute('id');
 
@@ -23,17 +20,34 @@ window.onscroll = () => {
                                     links.classList.remove('active');
                                     document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
                            });
-                           sec.classList.add('show-animate');
-                  } else {
-                           sec.classList.remove('show-animate');
-                  }
 
+                  };
          });
+
          let header = document.querySelector('header');
+
          header.classList.toggle('sticky', window.scrollY > 100);
 
          menuIcon.classList.remove('bx-x');
          navbar.classList.remove('active');
+};
 
+ScrollReveal({
+         reset: true,
+         distance: '80px',
+         duration: 2000,
+         delay: 200
+});
 
-}
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container , portfolio-box , .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+         strings : ['Frontend Developer', 'Backend Developer', 'Game Developer'],
+         typeSpeed : 100,
+         backSpeed : 100,
+         backDelay : 1000,
+         loop : true
+});
