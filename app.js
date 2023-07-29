fd2e66496796e97bfe1e526fdf4dab4cb07f6c49
Selim Kaya <136734877+selimthebrogrammer@users.noreply.github.com>
2023-07-29 17:34:32 +0300

    app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-         strings : ['Frontend Developer', 'Backend Developer', 'Game Developer'],
+         strings : ['Frontend Developer', 'Backend Developer', 'Fullstack Developer'],
          typeSpeed : 100,
          backSpeed : 100,
          backDelay : 1000,
