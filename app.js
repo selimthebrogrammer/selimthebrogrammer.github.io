6432cfbf65e1b5f3efbcbc4e29bd3ff5cff499a5
Selim Kaya <136734877+selimthebrogrammer@users.noreply.github.com>
2023-08-04 17:43:31 +0300

    app.js

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ ScrollReveal({
          delay: 200
 });
 
+
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container , portfolio-box , .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
