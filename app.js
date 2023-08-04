7e71673296a5708c5236db2d649ab4cf37e2fcf2
Selim Kaya <136734877+selimthebrogrammer@users.noreply.github.com>
2023-08-04 17:43:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+
 window.onscroll = () => {
          sections.forEach(sec => {
                   let top = window.scrollY;
