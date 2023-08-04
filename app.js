7be78482d1737c40a7f76768dff122c48817bde4
Selim Kaya <136734877+selimthebrogrammer@users.noreply.github.com>
2023-08-04 17:41:56 +0300

    app.js

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ window.onscroll = () => {
                            navLinks.forEach(links => {
                                     links.classList.remove('active');
                                     document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                                    
                            });
 
                   };
          });
 
-         
          let header = document.querySelector('header');
 
          header.classList.toggle('sticky', window.scrollY > 100);
