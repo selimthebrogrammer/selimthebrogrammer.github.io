376cef423ebe72a64bfbfeb9b857ff04ddf88af9
Selim Kaya <136734877+selimthebrogrammer@users.noreply.github.com>
2023-08-04 17:43:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+
 menuIcon.onclick = () => {
          menuIcon.classList.toggle('bx-x');
          navbar.classList.toggle('active');
